150ac60ce8a60c106f6861080a5479e17079fe6f
Devin Brown <devin.g.brown@gmail.com>
2018-09-20 06:47:17 -0600

    update serve script to locally with webpack + nginx

--- applications/web/scripts/deploy.sh
+++ applications/web/scripts/deploy.sh
@@ -6,10 +6,10 @@ set -efux
 #   use builder container
 #   copy and build in server container
 
-node_modules/.bin/webpack --mode=production --output-path=public
-
-docker build . --file Dockerfile --tag originalhat/typescript-ref-arch
-
+#node_modules/.bin/webpack --mode=production --output-path=public
+#
+#docker build . --file Dockerfile --tag originalhat/typescript-ref-arch
+#
 # docker push originalhat/typescript-ref-arch:latest
-
+#
 # cf push typescript-ref-arch --docker-image originalhat/typescript-ref-arch
--- applications/web/scripts/serve.sh
+++ applications/web/scripts/serve.sh
@@ -2,16 +2,9 @@
 
 set -efux
 
-# TODO:
-#   this should use the builder container;
-#   mount local file system;
-#   run webpack then pass to server container;
-#   avoid detaching;
-#   properly terminate;
-#   format webpack output
-
-docker build . --file Dockerfile --tag originalhat/typescript-ref-arch
-
-docker run -v /Users/pipeline/workspace/typescript-ref-arch/public:/usr/share/nginx/html --detach --publish 8080:80 originalhat/typescript-ref-arch
+docker run \
+    -- /Users/pipeline/workspace/ref-architecture/applications/web/public:/usr/share/nginx/html \
+    --detach \
+    --publish 8080:80 nginx:latest
 
 node_modules/.bin/webpack --mode=development --output-path=public --watch
